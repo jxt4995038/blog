c809c1cc1bd402d642b91e3725b5ac0572ada4ad
jiaxiaotong <799605395@qq.com>
2017-12-07 10:25:17 +0800

    提交无关东西

---
+++ src/test/java/ThreadTest/MainTets.java
@@ -0,0 +1,12 @@
+package ThreadTest;
+
+/**
+ * Created by Administrator on 2017/11/24 0024.
+ */
+public class MainTets {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.start();
+        System.out.println("执行方法");
+    }
+}
